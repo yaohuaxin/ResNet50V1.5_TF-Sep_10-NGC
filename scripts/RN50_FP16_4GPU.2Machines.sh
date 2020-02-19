31f7710fe7d8372fb131d5954bb938c3a4e9b7a2
Huaxin Yao <yao.huaxin@qq.com>
2020-02-19 11:11:07 +0800

    running on minsky

--- scripts/RN50_FP16_4GPU.2Machines.sh
+++ scripts/RN50_FP16_4GPU.2Machines.sh
@@ -15,5 +15,5 @@
 # This script launches ResNet50 training in FP16 on 8 GPUs using 2048 batch size (256 per GPU)
 # Usage ./RN50_FP16_8GPU.sh <path to this repository> <path to dataset> <path to results directory>
 
-mpiexec --allow-run-as-root -pami_noib --bind-to socket -np 4 \
+ddlrun -tcp --mpiarg "--allow-run-as-root --mca btl_tcp_if_include 10.30.0.0/24" --accelerators 4  --host "10.30.0.127:4,10.30.0.129:4" \
 python $1/main.py --mode=train_and_evaluate --iter_unit=epoch --num_iter=50 --batch_size=128 --warmup_steps=100 --use_cosine_lr --label_smoothing 0.1 --lr_init=0.256 --lr_warmup_epochs=8 --momentum=0.875 --weight_decay=3.0517578125e-05 --use_tf_amp  --data_dir=$2 --results_dir=$3
